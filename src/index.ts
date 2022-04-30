37056b35f5a75d07ea5389faad839c7fe11fe71d
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2022-04-30 11:27:48 +0200

    Mongo db configured and connecting properly

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
+dotenv.config();
 import './setUpDB';
 
 const app = express();
 //Body parser middleware
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use('/', router);
 // Set port and have server listen
 const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
